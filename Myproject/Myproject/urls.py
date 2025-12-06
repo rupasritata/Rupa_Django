8aeadb7540dd49445c18cf8f1f53f47767bb51f5
rupasritata <rupasritata@gmail.com>
2025-12-06 10:59:26 +0530

    requirements.txt added

--- Myproject/Myproject/urls.py
+++ Myproject/Myproject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from basic.views import home,about,contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',home),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact')
 ]
